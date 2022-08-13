820fb988130b4c4a9f7d8e8000cf85bab321c8a1
Bernardo Enock <bernardoenockgm@gmail.com>
2022-08-13 13:56:14 -0300

    feat: up volumes docker compose

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,13 +1,23 @@
 import { Client } from "pg";
 import "dotenv/config";
 
-const database = new Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB,
-  port: 5432,
-});
+const database = new Client(
+  process.env.NODE_ENV === "test"
+    ? {
+        user: "postgres",
+        password: "1234",
+        host: "localhost",
+        database: "test_imemes",
+        port: 5432,
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        database: process.env.DB,
+        port: 5432,
+      }
+);
 
 export const startDatabase = async () => {
   await database.connect();
